7a2d9ebd2024833f215a5667b8fac14b0c5207f7
kuzeng <kuiduanz@gmail.com>
2024-08-02 13:51:02 -0700

    first commit

--- app/page.js
+++ app/page.js
@@ -1,95 +1,171 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client';
+import { useState, useEffect } from "react";
+import { firestore } from "../firebase";
+import { Box, Modal, Typography, Stack, TextField, Button } from "@mui/material";
+import { collection, getDocs, query, doc, getDoc, setDoc, deleteDoc } from "firebase/firestore";
 
 export default function Home() {
+  const [inventory, setInventory] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [itemName, setItemName] = useState("");
+
+  const updateInventory = async () => {
+    const q = query(collection(firestore, "inventory"));
+    const docs = await getDocs(q);
+    const inventoryList = [];
+    docs.forEach((doc) => {
+      inventoryList.push({
+        name: doc.id,
+        ...doc.data(),
+      });
+    });
+    setInventory(inventoryList);
+  };
+
+  const removeItem = async (name) => {
+    const docRef = doc(collection(firestore, "inventory"), name);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+      const { quantity } = docSnap.data();
+      if (quantity === 1) {
+        await deleteDoc(docRef);
+      } else {
+        await setDoc(docRef, {
+          quantity: quantity - 1,
+        });
+      }
+    }
+
+    await updateInventory();
+  };
+
+  const addItem = async (name) => {
+    const docRef = doc(collection(firestore, "inventory"), name);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+      await setDoc(docRef, {
+        quantity: docSnap.data().quantity + 1,
+      });
+    } else {
+      await setDoc(docRef, {
+        quantity: 1,
+      });
+    }
+
+    await updateInventory();
+  };
+
+  useEffect(() => {
+    updateInventory();
+  }, []);
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
+    <Box
+      width="100vw"
+      height="100vh"
+      display="flex"
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+      gap={2}
+    >
+      <Modal open={open} handleClose={handleClose}>
+        <Box
+          position="absolute"
+          top="50%"
+          left="50%"
+          width={400}
+          bgcolor="white"
+          border="2px solid black"
+          boxShadow={24}
+          p={4}
+          display="flex"
+          flexDirection="column"
+          gap={3}
+          sx={{
+            transform: "translate(-50%, -50%)",
+          }}
+        >
+          <Typography variant="h6">Add Item</Typography>
+          <Stack width="100%" direction="row" spacing={2}>
+            <TextField
+              variant="outlined"
+              fullWidth
+              value={itemName}
+              onChange={(e) => setItemName(e.target.value)}
             />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
+            <Button variant="outlined" onClick={() => {
+              addItem(itemName);
+              setItemName("");
+              handleClose();
+            }}>Add</Button>
+          </Stack>
+        </Box>
+      </Modal>
+      <Box border="1px solid #333">
+        <Box
+          width="800px"
+          height="100px"
+          bgcolor="#ADD8E6"
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          borderBottom="1px solid #333"
         >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
+          <Typography variant="h2" color="#333">
+            Inventory Tracker
+          </Typography>
+        </Box>
+        <Box
+          width="100%"
+          height="50px"
+          display="flex"
+          alignItems="center"
+          justifyContent="space-between"
+          borderBottom="1px solid #333"
+          p={2}
         >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
+          <Typography variant="h6">Item</Typography>
+          <Typography
+            variant="h6"
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
+            sx={{
+              transform: "translate(-100%, 0%)",
+            }}
+          >Quantity</Typography>
+          <Typography></Typography>
+        </Box>
+        <Stack width="800px" height="300px" spacing={2} overflow="auto">
+          {
+            inventory.map((item) => (
+              <Box
+                key={item.name}
+                width="100%"
+                height="60px"
+                display="flex"
+                alignItems="center"
+                justifyContent="space-between"
+                borderBottom="1px solid #333"
+                p={2}
+              >
+                <Typography variant="body1">{item.name.charAt(0).toUpperCase() + item.name.slice(1)}</Typography>
+                <Typography variant="body1">{item.quantity}</Typography>
+                <Stack direction="row" spacing={2}>
+                  <Button variant="outlined" onClick={() => addItem(item.name)}>Add</Button>
+                  <Button variant="outlined" onClick={() => removeItem(item.name)}>Remove</Button>
+                </Stack>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+              </Box>
+            ))
+          }
+        </Stack>
+      </Box>
+      <Button variant="contained" onClick={() => handleOpen()}>Add New Item</Button>
+    </Box>
   );
 }
---
+++ firebase.js
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA2aCMppffbJYRcBIt9AW9aCEUZVV9Rgbw",
+  authDomain: "inventory-management-9fa11.firebaseapp.com",
+  projectId: "inventory-management-9fa11",
+  storageBucket: "inventory-management-9fa11.appspot.com",
+  messagingSenderId: "261540313202",
+  appId: "1:261540313202:web:e4f1b097f7e292bda66aeb",
+  measurementId: "G-X08QXZ5YNY"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const firestore = getFirestore(app);
+
+export { firestore };
